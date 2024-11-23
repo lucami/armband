234d6d9bd5d1693f44e4f5eb7e2eb5ff660feef2
waste <lucaminotti1988@gmail.com>
2024-11-23 18:39:38 +0100

    7 color configuration Blink blue led on the PCB Send incremental counter to 192.168.1.14

--- firmware/listener.py
+++ firmware/listener.py
@@ -1,8 +1,10 @@
 import socket
 import os
+import time
 
-def print_values(s1,s2):
+def print_values(s1,s2, t):
 	os.system("clear")
+	print(int(time.time()-t))
 	print(s1)
 	print(s2)
 
@@ -16,11 +18,11 @@ def update (s, s1,s2):
 
 s1=""
 s2=""
-
+start = time.time()
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("0.0.0.0", 6660))
 
 while True:
 	data, addr = s.recvfrom(1024) # buffer size is 1024 bytes
 	s1,s2 = update(data.decode("utf-8").strip().rstrip() , s1, s2)
-	print_values(s1,s2)
+	print_values(s1,s2, start)
--- firmware/main.py
+++ firmware/main.py
@@ -2,28 +2,42 @@ import time
 import machine
 import network
 import socket
+import neopixel
 
 port_dest = 6660
 addr_dest = '192.168.1.14'
 dest = (addr_dest, port_dest)
-
+np = neopixel.NeoPixel(machine.Pin(0, machine.Pin.OUT), 1) 
 f=open("ID","r")
 id = f.read(32)
 f.close()
-
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 led = machine.Pin(2, machine.Pin.OUT)
-
 i=0
 
 while 1:
+    #print(f"i: {i}")
     if led.value():
         led.off()
     else:
         led.on()
-    
-    
+
+    if i%7 == 0:
+        np[0] = (255, 0, 0) #Rosso
+    elif i%7 == 1:
+        np[0] = (0, 255, 0) #Verde
+    elif i%7 == 2:
+        np[0] = (0, 0, 255) #Blue
+    elif i%7 == 3:
+        np[0] = (0, 255, 255) #Ciano
+    elif i%7 == 4:
+        np[0] = (255, 255, 255) #Bianco
+    elif i%7 == 5:
+        np[0] = (255, 255,0) #Giallo
+    elif i%7 == 6:
+        np[0] = (255, 0,255) #Magenta
+
+    np.write()
     s.sendto(str(i)+" "+id+"\n",dest)
     i+=1
     time.sleep(1)
